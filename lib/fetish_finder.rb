b624bdf69f5d33cc1ae99abb8ba46c7225dbf104
Kevin Fitzpatrick <pair+kfitz@timelinelabs.com>
2014-09-05 12:17:47 -0700

    List out all friend from first page

--- lib/fetish_finder.rb
+++ lib/fetish_finder.rb
@@ -9,6 +9,34 @@ class FetishFinder
     fetish_intersection(user_a, user_b)
   end
 
+  def users_friends(user_id)
+    # for all pages, get the user's friends
+    authenticate!
+    first_friends_page = @mechanize.get("https://fetlife.com/users/#{user_id}/friends")
+    friend_links = first_friends_page.links_with(href: %r{/users/\d+})
+    #friend_links.map { |link| [link.text, link.href.match(/(\d+)/)[[1]]] }
+    tuples = friend_links.map { |link| [link.text, link.href] }
+    uniq_tuples = tuples.uniq!
+
+    only_friend_links = uniq_tuples.reject do |tuple|
+      # reject when: not a user link
+      # or
+      # is authenticated user
+      # or
+      # is current user
+      !tuple[1].match(%r{/users/\d+$}) ||
+        tuple[0].match(%r{View Your Profile}) ||
+        tuple[1].match(%r{/users/#{user_id}})
+    end
+
+    only_friend_links.map do |tuple|
+      [
+        tuple[0],
+        tuple[1].match(%r{/users/(\d+)})[1]
+      ]
+    end
+  end
+
   private
 
   def authenticate!
--- main.rb
+++ main.rb
@@ -10,10 +10,26 @@ require 'fetish_finder'
 
 config = YAML.load_file('config/credentials.yml')
 
-
 finder = FetishFinder.new(config)
-intersection = finder.in_common(config["testuser"], config["testfriend"])
 
-puts intersection.map { |f| f[0] }
+friends = finder.users_friends(config["testuser"])
+
+friend_intersections = friends.map do |friend|
+  intersection = finder.in_common(config["testuser"], friend[1])
+  puts friend[0]
+  puts intersection.map {|f|f[0]}
+  puts "-"*10
+  {
+    name: friend[0],
+    intersection: intersection
+  }
+end
+
+
+# Find all the friends of the user
+# compare all of their fetishes
+# Cache users' fetishes to not spam the original user's page
+
+#puts intersection.map { |f| f[0] }
 #binding.pry
 
